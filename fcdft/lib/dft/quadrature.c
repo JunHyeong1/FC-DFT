b96a7a57b0f5edd4252e348ec2648bf61cbb3876
Jun-Hyeong <jk590@duke.edu>
2025-06-09 14:55:09 -0400

    Generalized occ and occ_grad to support spin-unrestricted case

--- fcdft/lib/dft/quadrature.c
+++ fcdft/lib/dft/quadrature.c
@@ -34,30 +34,26 @@ void roots_legendre(int n, double *abscissas, double *weights){
 
 double occ_drv(double sampling, double moe_energy, double fermi, double broad, double smear) {
     double dist = 1 / (exp((sampling - fermi)/smear) + 1);
-    return dist * broad / (pow(sampling - moe_energy, 2) + pow(broad / 2, 2)) / M_PI;
+    return dist * broad / (pow(sampling - moe_energy, 2) + pow(broad / 2, 2)) / 2.0 / M_PI;
 }
 
 double occ_grad_drv(double sampling, double moe_energy, double fermi, double broad, double smear) {
     double dist = 1 / (exp((sampling - fermi)/smear) + 1);
-    return dist * (1 - dist) * broad / (pow(sampling - moe_energy, 2) + pow(broad / 2, 2)) / M_PI / smear;
+    return dist * (1 - dist) * broad / (pow(sampling - moe_energy, 2) + pow(broad / 2, 2)) / 2.0 / M_PI / smear;
 }
 
-void fermi_level_drv(double *moe_energy, double *abscissas, double *weights, double fermi, double broad, double smear, double window, int pts, int nbas, double *mo_occ, double *ni_grad) {
+void fermi_level_drv(double *moe_energy, double *abscissas, double *weights, double fermi, double broad, double smear, double window, int pts, int nbas, double *mo_occ, double *mo_grad) {
     int i, n;
-    double *_ni_grad = malloc(sizeof(double) * nbas * pts);
     double sampling;
-    #pragma omp parallel for private(i, n, sampling)
+    double _mo_grad = 0.0;
+    #pragma omp parallel for private(n, sampling) reduction(+:_mo_grad)
     for (i = 0; i < nbas; i++) {
         mo_occ[i] = 0;
         for (n = 0; n < pts; n++) {
             sampling = abscissas[n] * window + moe_energy[i];
             mo_occ[i] += window * weights[n] * occ_drv(sampling, moe_energy[i], fermi, broad, smear);
-            _ni_grad[i*pts + n] = window * weights[n] * occ_grad_drv(sampling, moe_energy[i], fermi, broad, smear);
+            _mo_grad += window * weights[n] * occ_grad_drv(sampling, moe_energy[i], fermi, broad, smear);
         }
     }
-    #pragma omp critical
-    for (i = 0; i < nbas * pts; i++) {
-        *ni_grad += _ni_grad[i];
-    }
-    free(_ni_grad);
+    *mo_grad = _mo_grad;
 }
