3894206ad7637c90c67960b04afe02416bd1547b
Jun-Hyeong <jk590@duke.edu>
2026-04-04 21:07:50 -0400

    Header file update

--- fcdft/lib/pbe/boundary_condition.h
+++ fcdft/lib/pbe/boundary_condition.h
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
-void grad_sas_drv(double *erf_list, double *grad_list, double *x, double delta2, int ngrids, int natm, double *grad_sas);
+void grad_sas_drv(double *erf_list, double *grad_list, double delta2, int ngrids, int natm, double *grad_sas);
 
 void lap_sas_drv(double *erf_list, double *grad_list, double *x, double delta2, int ngrids, int natm, double *lap_sas);
 
-void grad_eps_drv(double *erf_list, double *grad_list, double *x, double *exp_z, double *eps_z, double delta1, double delta2, double eps, double eps_sam, int ngrids, int natm, double *grad_eps);
+void grad_eps_drv(double *erf_list, double *grad_list, double *exp_z, double *eps_z, double delta1, double delta2, double eps, double eps_sam, int ngrids, int natm, double *grad_eps);
