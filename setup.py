6e0010e717c34bf4eff57fb88bf1c5b4d8e479e5
Jun-Hyeong <jk590@duke.edu>
2026-04-04 21:03:56 -0400

    Temporal Cython support

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
 from setuptools.extension import Extension
+from Cython.Build import cythonize
 import numpy
 import fcdft
 import os
 import sys
 
+extensions = [
+    Extension(
+        "fcdft.lib.pbe_helper",
+        ["fcdft/lib/pbe_helper.pyx"],
+        extra_compile_args=["-fopenmp"],
+        extra_link_args=["-fopenmp"],
+        include_dirs=[numpy.get_include()],
+    ),
+]
 # Copied from PySCF
 def get_platform():
     from distutils.util import get_platform
@@ -65,5 +75,6 @@ setup(
     version=fcdft.__version__,
     packages=find_packages(),
     include_package_data=True,
+    ext_modules = cythonize(extensions),
     cmdclass={'build_py': CMakeBuildPy},
 )
