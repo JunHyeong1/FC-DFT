a62a38a1ab86e0e574e7d08500a927cde78abec1
Jun-Hyeong <jk590@duke.edu>
2026-04-08 14:30:44 -0400

    test_get_jk under development (threshold for mo_occ)

--- fcdft/df/grad/rks.py
+++ fcdft/df/grad/rks.py
@@ -57,7 +57,7 @@ def get_jk(mf_grad, mol=None, dm=None, hermi=0, with_j=True, with_k=True,
     vk = numpy.zeros((nset,3,nao,nao), dtype=numpy.complex128)
     get_int3c_ip1 = df_rhf_grad._int3c_wrapper(mol, auxmol, 'int3c2e_ip1', 's1')
     max_memory = mf_grad.max_memory - lib.current_memory()[0]
-    blksize = int(min(max(max_memory * .5e6/8 / (nao**2*3), 20), naux, 80)) # 240 -> 80 for handling complex128
+    blksize = int(min(max(max_memory * .5e6/8 / (nao**2*3) / 2, 20), naux, 240)) # divided by 2 for handling complex128
     ao_ranges = df_rhf_grad.balance_partition(aux_loc, blksize)
     fmmm = _ao2mo.libao2mo.AO2MOmmm_bra_nr_s1 # MO output index slower than AO output index; input AOs are asymmetric
     fdrv = _ao2mo.libao2mo.AO2MOnr_e2_drv # comp and aux indices are slower
@@ -121,7 +121,214 @@ def get_jk(mf_grad, mol=None, dm=None, hermi=0, with_j=True, with_k=True,
     max_memory = mf_grad.max_memory - lib.current_memory()[0]
     # blksize = int(min(max(max_memory * .5e6/8 / (nao*max (nocc)), 20), naux))
     # In principe, all occupation numbers are non-zero for open quantum systems.
-    blksize = int(min(max(max_memory * .5e6/8 / (nao*nao), 20), naux) / 3) # Divided by 3 for handling complex128
+    blksize = int(min(max(max_memory * .5e6/8 / (nao*nao) / 2, 20), naux)) # Divided by 2 for handling complex128
+    rhok_oo = []
+    for i, j in product (range (nset), repeat=2):
+        tmp = numpy.empty ((naux,nocc[i],nocc[j]), dtype=numpy.complex128)
+        for p0, p1 in lib.prange(0, naux, blksize):
+            rhok = get_rhok (i, p0, p1).reshape ((p1-p0)*nocc[i], nao)
+            tmp[p0:p1] = lib.dot (rhok, orbol[j]).reshape (p1-p0, nocc[i], nocc[j])
+        rhok_oo.append(tmp)
+        rhok = tmp = None
+    t1 = logger.timer_debug1 (mf_grad, 'df grad vj and vk aux d_Pim u_mj = d_Pij eval', *t1)
+
+    vjaux = numpy.zeros((nset,nset,3,naux), dtype=numpy.complex128)
+    vkaux = numpy.zeros((nset,nset,3,naux), dtype=numpy.complex128)
+    # (i,j|d/dX P)
+    t2 = t1
+    get_int3c_ip2 = df_rhf_grad._int3c_wrapper(mol, auxmol, 'int3c2e_ip2', 's2ij')
+    fmmm = _ao2mo.libao2mo.AO2MOmmm_bra_nr_s2 # MO output index slower than AO output index; input AOs are symmetric
+    fdrv = _ao2mo.libao2mo.AO2MOnr_e2_drv # comp and aux indices are slower
+    ftrans = _ao2mo.libao2mo.AO2MOtranse2_nr_s2 # input is tril_packed
+    null = lib.c_null_ptr()
+    for shl0, shl1, nL in ao_ranges:
+        int3c = get_int3c_ip2((0, nbas, 0, nbas, shl0, shl1))  # (i,j|P)
+        t2 = logger.timer_debug1 (mf_grad, "df grad intor (P'|mn)", *t2)
+        p0, p1 = aux_loc[shl0], aux_loc[shl1]
+        drhoj = lib.dot (int3c.transpose (0,2,1).reshape (3*(p1-p0), -1),
+            dm_tril.T).reshape (3, p1-p0, -1) # xpij,mij->xpm
+        vjaux[:,:,:,p0:p1] = lib.einsum ('xpm,np->mnxp', drhoj, rhoj[:,p0:p1])
+        t2 = logger.timer_debug1 (mf_grad, "df grad vj aux (P'|mn) eval", *t2)
+        # MRH, 09/19/2022: This is a different order of operations than PySCF v2.1.0. There,
+        #                  the dense matrix rhok_oo is transformed into the larger AO basis.
+        #                  Here, the sparse matrix int3c is transformed into the smaller MO
+        #                  basis. The latter approach is obviously more performant.
+        for i in range (nset):
+            buf = numpy.empty((3, p1-p0, nocc[i], nao), dtype=numpy.complex128)
+            bufRe = numpy.empty((3, p1-p0, nocc[i], nao), dtype=numpy.float64)
+            bufIm = numpy.empty((3, p1-p0, nocc[i], nao), dtype=numpy.float64)
+            orbolRe = numpy.asarray(orbol[i].real, order='F')
+            orbolIm = numpy.asarray(orbol[i].imag, order='F')
+            fdrv(ftrans, fmmm, # lib.einsum ('pmn,ni->pim', int3c, orbol[i])
+                    bufRe.ctypes.data_as(ctypes.c_void_p),
+                    int3c.ctypes.data_as(ctypes.c_void_p),
+                    orbolRe.ctypes.data_as(ctypes.c_void_p),
+                    ctypes.c_int (3*(p1-p0)), ctypes.c_int (nao),
+                    (ctypes.c_int*4)(0, nocc[i], 0, nao),
+                    null, ctypes.c_int(0))
+            fdrv(ftrans, fmmm, # lib.einsum ('pmn,ni->pim', int3c, orbol[i])
+                    bufIm.ctypes.data_as(ctypes.c_void_p),
+                    int3c.ctypes.data_as(ctypes.c_void_p),
+                    orbolIm.ctypes.data_as(ctypes.c_void_p),
+                    ctypes.c_int (3*(p1-p0)), ctypes.c_int (nao),
+                    (ctypes.c_int*4)(0, nocc[i], 0, nao),
+                    null, ctypes.c_int(0))
+            buf.real, buf.imag = bufRe, bufIm
+            for j in range (nset): # lib.einsum ('pim,mj->pij', buf, orbor[j])
+                int3c_ij = lib.dot (buf.reshape (-1, nao), orbor[j])
+                int3c_ij = int3c_ij.reshape (3, p1-p0, nocc[i], nocc[j])
+                rhok_oo_ij = rhok_oo[(i*nset)+j][p0:p1]
+                vkaux[i,j,:,p0:p1] += lib.einsum('xpij,pij->xp', int3c_ij,
+                                                 rhok_oo_ij)
+        int3c = None
+        t2 = logger.timer_debug1 (mf_grad, "df grad vk aux (P'|mn) eval", *t2)
+    orbol = orbor = None
+    buf = bufRe = bufIm = orbolRe = orbolIm = None
+    dm_tril = None
+    int3c = tmp = None
+    t1 = logger.timer_debug1 (mf_grad, "df grad vj and vk aux (P'|mn) eval", *t1)
+
+    # (d/dX P|Q)
+    int2c_e1 = auxmol.intor('int2c2e_ip1')
+    vjaux -= lib.einsum('xpq,mp,nq->mnxp', int2c_e1, rhoj, rhoj)
+    for i, j in product (range (nset), repeat=2):
+        k = (i*nset) + j
+        l = (j*nset) + i
+        tmp = numpy.zeros((naux,naux), dtype=numpy.complex128)
+        for p0, p1 in lib.prange(0, nao, blksize):
+            tmp += numpy.tensordot(rhok_oo[k][:,p0:p1,:], rhok_oo[l][:,:,p0:p1], axes=([1,2], [2,1]))
+        vkaux[i,j] -= lib.einsum('xpq,pq->xp', int2c_e1, tmp)
+    t1 = logger.timer_debug1 (mf_grad, "df grad vj and vk aux (P'|Q) eval", *t1)
+
+    auxslices = auxmol.aoslice_by_atom()
+    vjaux = numpy.array ([-vjaux[:,:,:,p0:p1].sum(axis=3) for p0, p1 in auxslices[:,2:]])
+    vkaux = numpy.array ([-vkaux[:,:,:,p0:p1].sum(axis=3) for p0, p1 in auxslices[:,2:]])
+
+    vjaux = numpy.ascontiguousarray (vjaux.transpose (1,2,0,3))
+    vkaux = numpy.ascontiguousarray (vkaux.transpose (1,2,0,3))
+
+    vj = lib.tag_array(-vj.reshape(out_shape), aux=numpy.array(vjaux))
+    vk = lib.tag_array(-vk.reshape(out_shape), aux=numpy.array(vkaux))
+    logger.timer (mf_grad, 'df grad vj and vk', *t0)
+    if with_j: return vj, vk
+    else: return None, vk
+
+# This function needs to pass consistent test.
+def test_get_jk(mf_grad, mol=None, dm=None, hermi=0, with_j=True, with_k=True,
+           decompose_j2c='CD', lindep=LINEAR_DEP_THRESHOLD):
+    """Copied from pyscf.df.grad.rhf and some parts modified to handle complex density matrices."""
+    assert (with_j or with_k)
+    if not with_k:
+        return get_j (mf_grad, mol=mol, dm=dm, hermi=hermi), None
+    t0 = (logger.process_clock (), logger.perf_counter ())
+    if mol is None: mol = mf_grad.mol
+
+    # Ignore mo occpuation less than 1.0e-3.
+    # The error is less than 0.1% despite significant memory saving.
+    mo_coeff = mf_grad.base.mo_coeff
+    mo_occ   = mf_grad.base.mo_occ.copy()
+    mo_occ[mo_occ < 1e-3] = 0.0e0
+    dm = mf_grad.base.make_rdm1(mo_coeff, mo_occ)
+    
+    with_df = mf_grad.base.with_df
+    auxmol = with_df.auxmol
+    if auxmol is None:
+        auxmol = df.addons.make_auxmol(with_df.mol, with_df.auxbasis)
+    nbas, nao, naux = mol.nbas, mol.nao, auxmol.nao
+    aux_loc = auxmol.ao_loc
+
+    # Density matrix preprocessing
+    dms = numpy.asarray(dm)
+    out_shape = dms.shape[:-2] + (3,) + dms.shape[-2:]
+    dms = dms.reshape(-1,nao,nao)
+    nset = dms.shape[0]
+
+    # For j
+    idx = numpy.arange(nao)
+    idx = idx * (idx+1) // 2 + idx
+    dm_tril = dms + dms.transpose(0,2,1)
+    dm_tril = lib.pack_tril(dm_tril)
+    dm_tril[:,idx] *= .5
+
+    # For k
+    orbol, orbor = df_rhf_grad._decompose_rdm1_svd (mf_grad, mol, dm)
+    nocc = [o.shape[-1] for o in orbor]
+
+    # Coulomb: (P|Q) D_Q = (P|uv) D_uv for D_Q ("rhoj")
+    # Exchange: (P|Q) D_Qui = (P|uv) C_vi n_i for D_Qui ("rhok")
+    rhoj, get_rhok = _cho_solve_rhojk (mf_grad, mol, auxmol, orbol, orbor,
+                                       decompose_j2c, lindep)
+
+    # (d/dX i,j|P)
+    t1 = (logger.process_clock (), logger.perf_counter ())
+    vj = numpy.zeros((nset,3,nao,nao), dtype=numpy.complex128)
+    vk = numpy.zeros((nset,3,nao,nao), dtype=numpy.complex128)
+    get_int3c_ip1 = df_rhf_grad._int3c_wrapper(mol, auxmol, 'int3c2e_ip1', 's1')
+    max_memory = mf_grad.max_memory - lib.current_memory()[0]
+    blksize = int(min(max(max_memory * .5e6/8 / (nao**2*3), 20), naux, 240))
+    ao_ranges = df_rhf_grad.balance_partition(aux_loc, blksize)
+    fmmm = _ao2mo.libao2mo.AO2MOmmm_bra_nr_s1 # MO output index slower than AO output index; input AOs are asymmetric
+    fdrv = _ao2mo.libao2mo.AO2MOnr_e2_drv # comp and aux indices are slower
+    ftrans = _ao2mo.libao2mo.AO2MOtranse2_nr_s1 # input is not tril_packed
+    null = lib.c_null_ptr()
+    t2 = t1
+    for shl0, shl1, nL in ao_ranges:
+        int3c = get_int3c_ip1((0, nbas, 0, nbas, shl0, shl1)).transpose (0,3,2,1)  # (P|mn'), row-major order
+        t2 = logger.timer_debug1 (mf_grad, "df grad intor (P|mn')", *t2)
+        p0, p1 = aux_loc[shl0], aux_loc[shl1]
+        for i in range(nset):
+            # MRH 05/21/2020: De-vectorize this because array contiguity -> multithread efficiency
+            vj[i,0] += numpy.dot (rhoj[i,p0:p1], int3c[0].reshape (p1-p0, -1)).reshape (nao, nao).T
+            vj[i,1] += numpy.dot (rhoj[i,p0:p1], int3c[1].reshape (p1-p0, -1)).reshape (nao, nao).T
+            vj[i,2] += numpy.dot (rhoj[i,p0:p1], int3c[2].reshape (p1-p0, -1)).reshape (nao, nao).T
+            t2 = logger.timer_debug1 (mf_grad, "df grad einsum rho_P (P|mn') rho_P", *t2)
+            tmp = numpy.empty ((3,p1-p0,nocc[i],nao), dtype=numpy.complex128)
+            tmpRe = numpy.empty ((3,p1-p0,nocc[i],nao), dtype=numpy.float64)
+            tmpIm = numpy.empty ((3,p1-p0,nocc[i],nao), dtype=numpy.float64)
+            orbolRe = numpy.asarray(orbol[i].real, order='F')
+            orbolIm = numpy.asarray(orbol[i].imag, order='F')
+            fdrv(ftrans, fmmm, # lib.einsum ('xpmn,mi->xpin', int3c, orbol[i])
+                 tmpRe.ctypes.data_as(ctypes.c_void_p),
+                 int3c.ctypes.data_as(ctypes.c_void_p),
+                 orbolRe.ctypes.data_as(ctypes.c_void_p),
+                 ctypes.c_int (3*(p1-p0)), ctypes.c_int (nao),
+                 (ctypes.c_int*4)(0, nocc[i], 0, nao),
+                 null, ctypes.c_int(0))
+            orbolRe = None
+            fdrv(ftrans, fmmm, # lib.einsum ('xpmn,mi->xpin', int3c, orbol[i])
+                 tmpIm.ctypes.data_as(ctypes.c_void_p),
+                 int3c.ctypes.data_as(ctypes.c_void_p),
+                 orbolIm.ctypes.data_as(ctypes.c_void_p),
+                 ctypes.c_int (3*(p1-p0)), ctypes.c_int (nao),
+                 (ctypes.c_int*4)(0, nocc[i], 0, nao),
+                 null, ctypes.c_int(0))
+            orbolIm = None
+            tmp.real, tmp.imag = tmpRe, tmpIm
+            tmpRe = tmpIm = None
+            t2 = logger.timer_debug1 (mf_grad, "df grad einsum (P|mn') u_mi = dg_Pin", *t2)
+            rhok = get_rhok (i, p0, p1)
+            vk[i] += numpy.tensordot(tmp, rhok, axes=([1,2],[0,1]))
+            t2 = logger.timer_debug1 (mf_grad, "df grad einsum D_Pim dg_Pin = v_ij", *t2)
+            rhok = tmp = None
+        int3c = None
+    t1 = logger.timer_debug1 (mf_grad, 'df grad vj and vk AO (P|mn) D_P eval', *t1)
+
+    if not mf_grad.auxbasis_response:
+        vj = -vj.reshape(out_shape)
+        vk = -vk.reshape(out_shape)
+        logger.timer (mf_grad, 'df grad vj and vk', *t0)
+        if with_j: return vj, vk
+        else: return None, vk
+
+    ####### BEGIN AUXBASIS PART #######
+
+    # ao2mo the final AO index of rhok and store in "rhok_oo":
+    # dPiu C_uj -> dPij. *Not* symmetric i<->j: "i" has an occupancy
+    # factor and "j" must not.
+    max_memory = mf_grad.max_memory - lib.current_memory()[0]
+    blksize = int(min(max(max_memory * .5e6/8 / (nao*max (nocc)), 20), naux))
+    # In principe, all occupation numbers are non-zero for open quantum systems.
+    # blksize = int(min(max(max_memory * .5e6/8 / (nao*nao), 20), naux))
     rhok_oo = []
     for i, j in product (range (nset), repeat=2):
         tmp = numpy.empty ((naux,nocc[i],nocc[j]), dtype=numpy.complex128)
